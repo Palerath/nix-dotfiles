1698bf2ed8ec9556a3bc12c9fcd321308ab1a419
perihelie <archibaldmk4@gmail.com>
2025-04-11 01:16:31 +0200

    shelly commit

---
+++ devices/perikon/modules/shell.nix
@@ -0,0 +1,19 @@
+{pkgs, ...}:
+{
+   users.extraUsers.perihelie = {
+      shell = pkgs.zsh;
+   };
+   users.users.perihelie.ignoreShellProgramCheck = true;
+
+   programs.bash = {
+      shellAliases =
+         let
+            flakePath = "/home/perihelie/.dotfiles";
+         in{
+            rebuild = "sudo nixos-rebuild switch --flake ${flakePath}";
+            hms = "home-manager switch --flake ${flakePath}";
+            nv = "nvim";
+
+         };
+   };
+}
