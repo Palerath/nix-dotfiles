aca63baca3d34ef2ef9515abb83221e004a7dd0d
perihelie <archibaldmk4@gmail.com>
2025-05-06 15:30:46 +0200

    hyprland2

--- flake.nix
+++ flake.nix
@@ -1,77 +1,78 @@
 {
-  description = "A very basic flake";
+   description = "A very basic flake";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    
-    home-manager = {
-	url = "github:nix-community/home-manager";
-	inputs.nixpkgs.follows = "nixpkgs";
-    };
+   inputs = {
+      nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
 
-    # darwin = {
-	# url = "github:nix-darwin/nix-darwin/master";
-	# inputs.nixpkgs.follows = "nixpkgs";
-    # };
+      home-manager = {
+         url = "github:nix-community/home-manager";
+         inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-    nvf = {
-	url = "github:notashelf/nvf";
-	inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland = {
-		url = "github:hyprwm/Hyprland";
-		inputs.nixpkgs.follows = "nixpkgs";
-    };
+      # darwin = {
+      # url = "github:nix-darwin/nix-darwin/master";
+      # inputs.nixpkgs.follows = "nixpkgs";
+      # };
 
-  };
+      nvf = {
+         url = "github:notashelf/nvf";
+         inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, nvf, hyprland, ... }:
-	let 
-		lib = nixpkgs.lib;
-		pkgs = nixpkgs.legacyPackages."x86_64-linux";
-	in
-	{
+      hyprland = {
+         url = "github:hyprwm/Hyprland";
+         inputs.nixpkgs.follows = "nixpkgs";
+      };
 
-	nixosConfigurations = {
-		perikon = lib.nixosSystem {
-			system = "x86_64-linux";
-			modules = [ 
-				./perikon/configuration.nix
-				# home-manager.nixosModules.home-manager {
-				# 	home-manager.useGlobalPkgs = true;
-				# 	home-manager.useUserPackages =true;
-				# 	home-manager.users.perihelie = ./users/perihelie/home.nix;
-					# home-manager.extraSpecialArgs = [];
-				# }
+   };
 
-			];
-		};
-	};
+   outputs = inputs@{ self, nixpkgs, home-manager, nvf, hyprland, ... }:
+      let 
+         lib = nixpkgs.lib;
+         pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in
+         {
 
-	# darwinConfigurations = {
-		# "perihelie@air" = darwin.lib.darwinSystem {
-			# modules = [ 
-				# ./devices/macbook-air/darwin-configuration.nix 
-				# home-manager.darwinModules.home-manager {
-					# home-manager.useGlobalPkgs = true;
-					# home-manager.useUserPackages = true;
-					# home-manager.users.perihelie = ./users/perihelie/home.nix;
+         nixosConfigurations = {
+            perikon = lib.nixosSystem {
+               system = "x86_64-linux";
+               modules = [ 
+                  ./perikon/configuration.nix
+                  # home-manager.nixosModules.home-manager {
+                  # 	home-manager.useGlobalPkgs = true;
+                  # 	home-manager.useUserPackages =true;
+                  # 	home-manager.users.perihelie = ./users/perihelie/home.nix;
+                  # home-manager.extraSpecialArgs = [];
+                  # }
 
-					# home-manager.extraSpecialArgs = [];
-				# }
-			# ];
-		# };
-	# };
+               ];
+            };
+         };
 
- 	homeConfigurations = {
- 	 	"perihelie" = home-manager.lib.homeManagerConfiguration {
- 			pkgs = nixpkgs.legacyPackages."x86_64-linux";
- 	 		modules = [ 
-				./users/perihelie/home.nix 
-				nvf.homeManagerModules.nvf
-			];
- 	 	};
- 	};
+         # darwinConfigurations = {
+         # "perihelie@air" = darwin.lib.darwinSystem {
+         # modules = [ 
+         # ./devices/macbook-air/darwin-configuration.nix 
+         # home-manager.darwinModules.home-manager {
+         # home-manager.useGlobalPkgs = true;
+         # home-manager.useUserPackages = true;
+         # home-manager.users.perihelie = ./users/perihelie/home.nix;
 
-  };
+         # home-manager.extraSpecialArgs = [];
+         # }
+         # ];
+         # };
+         # };
+
+         homeConfigurations = {
+            "perihelie" = home-manager.lib.homeManagerConfiguration {
+               pkgs = nixpkgs.legacyPackages."x86_64-linux";
+               modules = [ 
+                  ./users/perihelie/home.nix 
+                  nvf.homeManagerModules.nvf
+               ];
+            };
+         };
+
+      };
 }
