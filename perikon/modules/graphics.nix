032803cfe64756e7018a7c8e64c5eea4b8a9efba
perihelie <archibaldmk4@gmail.com>
2025-05-06 22:49:17 +0200

    gpu power management

--- perikon/modules/graphics.nix
+++ perikon/modules/graphics.nix
@@ -1,31 +1,31 @@
 {pkgs, ...}:
 {
-  # Enable graphics support
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.opengl = {
-	enable = true;
-	extraPackages = with pkgs;[
-		mesa
-		vulkan-loader
-	];
-  };
-
-  # Specify the NVIDIA driver
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  hardware.nvidia = {
-    open = true;
-    videoAcceleration = true;
-    # Enable modesetting
-    modesetting = {
+   # Enable graphics support
+   hardware.graphics = {
       enable = true;
-    };
+      enable32Bit = true;
+   };
 
-    # Enable the NVIDIA settings tool
-    nvidiaSettings = true;
-  };
+   hardware.opengl = {
+      enable = true;
+      extraPackages = with pkgs;[
+         mesa
+         vulkan-loader
+      ];
+   };
+
+   # Specify the NVIDIA driver
+   services.xserver.videoDrivers = [ "nvidia" ];
+
+   hardware.nvidia = {
+      open = true;
+      videoAcceleration = true;
+      powerManagement.enable = true;
+      powerManagement.finegrained = false;
+      modesetting = {
+         enable = true;
+      };
+
+      nvidiaSettings = true;
+   };
 }
