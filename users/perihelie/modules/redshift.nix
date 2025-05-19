3b2583495a16493cdc0315d7b258e929366ade84
perihelie <archibaldmk4@gmail.com>
2025-05-19 15:44:40 +0200

    redshifting to the infrareds2

--- users/perihelie/modules/redshift.nix
+++ users/perihelie/modules/redshift.nix
@@ -1,16 +1,30 @@
-{pkgs, ... }:
+{pkgs, lib, ... }:
 {
    services.redshift = {
       enable = true;
+      package = pkgs.redshift;
+      tray = true;
+      enableVerboseLogging  = false;    
       latitude = "49.433331";
       longitude = "1.08333";
-      temperature.day       = 6500;  
-      temperature.night     = 3400;
-      brightness.day        = "1.0";    
-      brightness.night      = "0.7";
-      extraOptions          = [ "-m" "randr" "-v" ];
-      package               = pkgs.redshift;
-      tray                  = true;
-      enableVerboseLogging  = false;     
+      # temperature.day       = 6500;  
+      # temperature.night     = 3400;
+      dawnTime = "07:00";
+      duskTime = "21:00";
+      settings = {
+         redshift = {
+            adjustement-method = "randr";
+            # brightness-day = "1.0";   
+            # brightness-night = "0.7";
+            temp-day = lib.mkDefault "6500";
+            temp-night = lib.mkDefault "3700";
+            transition = true;
+         };
+         randr = {
+            screen = 0;
+         };
+
+      };
+
    };
 }
