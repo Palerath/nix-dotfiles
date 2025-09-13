1d77167dd3f07ac27439be45a107538997e1fd33
perihelie <archibaldmk4@gmail.com>
2025-09-13 12:42:54 +0200

    removed rust and python now shell only

--- hosts/perikon/modules/devel.nix
+++ hosts/perikon/modules/devel.nix
@@ -2,16 +2,8 @@
 {
    environment.systemPackages = with pkgs; [
       # PYTHON
-      python3Full
       pipx
-      poetry
-
-      # RUST
-      rustup
-      rustc
-      rust-analyzer
-      cargo
-
+      
       # C/C++
       gcc
       clang
@@ -27,6 +19,5 @@
 
    environment.sessionVariables = {
       PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
-      RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
    };
 }
