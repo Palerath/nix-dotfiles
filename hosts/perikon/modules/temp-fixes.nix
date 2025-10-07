79d110ee5208f15283c2d90ce4ea9f2b3bca8e63
perihelie <archibaldmk4@gmail.com>
2025-10-07 16:08:15 +0200

    temp fixes

--- hosts/perikon/modules/temp-fixes.nix
+++ hosts/perikon/modules/temp-fixes.nix
@@ -1,21 +1,21 @@
 {
    # CMake < 3.5
 
+
    nixpkgs.overlays = [
-      (final: prev: 
-         let
-            # Helper function to add cmake policy flag
-            fixCmake = pkg: pkg.overrideAttrs (old: {
-               cmakeFlags = (old.cmakeFlags or []) ++ [
-                  "-DCMAKE_POLICY_VERSION_MINIMUM=3.5"
-               ];
-            });
-         in
-            {
-            argagg = fixCmake prev.argagg;
-            slop = fixCmake prev.slop;
-            allegro = fixCmake.prev.allegro;
-         }
-      )
+      (final: prev: {
+         argagg = prev.argagg.overrideAttrs (old: {
+            cmakeFlags = (old.cmakeFlags or []) ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
+         });
+
+         slop = prev.slop.overrideAttrs (old: {
+            cmakeFlags = (old.cmakeFlags or []) ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
+         });
+
+         allegro = prev.allegro.overrideAttrs (old: {
+            cmakeFlags = (old.cmakeFlags or []) ++ [ "-DCMAKE_POLICY_VERSION_MINIMUM=3.5" ];
+         });  
+      })
    ];
+
 }
