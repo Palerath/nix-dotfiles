e8f55f8e568c3b45fcfafba00517bd8d458e61a5
perihelie <archibaldmk4@gmail.com>
2025-10-07 15:47:00 +0200

    gayming.nix ???

--- hosts/perikon/modules/gayming.nix
+++ hosts/perikon/modules/gayming.nix
@@ -1,67 +1,66 @@
 { pkgs, ... }:
 {
 
-  environment.systemPackages = with pkgs; [
-    # Gaming platforms
-    lutris
-    bottles
-    ckan
-    rpcs3
-    pcsx2
-    ryubing
+   environment.systemPackages = with pkgs; [
+      # Gaming platforms
+      lutris
+      bottles
+      ckan
+      rpcs3
+      pcsx2
+      ryubing
 
-    # Steam utilities
-    gamescope
-    mangohud
-    goverlay
+      # Steam utilities
+      gamescope
+      mangohud
+      goverlay
 
-    # Wine and compatibility
-    wineWowPackages.unstable
-    winetricks
-    protonup-ng
-    protonup-qt
+      # Wine and compatibility
+      wineWowPackages.unstable
+      winetricks
+      protonup-ng
+      protonup-qt
 
-    # Vulkan tools
-    vulkan-tools
-    vulkan-headers
-    vulkan-loader
-    vulkan-validation-layers
-  ];
+      # Vulkan tools
+      vulkan-tools
+      vulkan-headers
+      vulkan-loader
+      vulkan-validation-layers
+   ];
 
-  programs.steam = {
-    enable = true;
-    remotePlay.openFirewall = true;
-    dedicatedServer.openFirewall = true;
-    localNetworkGameTransfers.openFirewall = true;
-    gamescopeSession.enable = true;
-    protontricks.enable = true;
-    # Additional Steam packages
-    extraCompatPackages = with pkgs; [
-      proton-ge-bin
-    ];
-  };
-
-  programs.gamemode = {
-    enable = true;
-    settings = {
-      general = {
-        renice = 10;
+   programs.steam = {
+      enable = true;
+      remotePlay.openFirewall = true;
+      dedicatedServer.openFirewall = true;
+      localNetworkGameTransfers.openFirewall = true;
+      gamescopeSession.enable = true;
+      protontricks.enable = true;
+      # Additional Steam packages
+      extraCompatPackages = with pkgs; [
+         proton-ge-bin
+      ];
+   };
+   programs.gamemode = {
+      enable = true;
+      settings = {
+         general = {
+            renice = 10;
+         };
+         gpu = {
+            apply_gpu_optimisations = "accept-responsibility";
+            gpu_device = 0;
+            amd_performance_level = "high";
+         };
       };
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 0;
-        amd_performance_level = "high";
-      };
-    };
-  };
+   };
 
-  environment.sessionVariables = {
-    PROTON_ENABLE_NVAPI = "1";
-    PROTON_ENABLE_NGX_UPDATER = "1";
-  };
+   environment.sessionVariables = {
+      PROTON_ENABLE_NVAPI = "1";
+      PROTON_ENABLE_NGX_UPDATER = "1";
+   };
 
-  services.udev.packages = with pkgs; [
-    game-devices-udev-rules
-  ];
+   services.udev.packages = with pkgs; [
+      game-devices-udev-rules
+   ];
 
 }
