ae1e3574ab9f8f92b8ec08b1d60d67d3f2aafa0e
perihelie <archibaldmk4@gmail.com>
2025-10-08 13:13:50 +0200

    more localing

--- hosts/perikon/modules/locales.nix
+++ hosts/perikon/modules/locales.nix
@@ -1,8 +1,14 @@
 {
    i18n.defaultLocale = "en_US.UTF-8";
+   i18n.extraLocales = [
+      "fr_FR.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+   ];
 
    i18n.extraLocaleSettings = {
+      LC_CTYPE = "en_US.UTF-8";
       LC_ADDRESS = "fr_FR.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
       LC_IDENTIFICATION = "fr_FR.UTF-8";
       LC_MEASUREMENT = "ja_JP.UTF-8";
       LC_MONETARY = "fr_FR.UTF-8";
