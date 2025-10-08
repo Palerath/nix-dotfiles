8f601acfb80465b9f55495649c18bd5bd0cb33cb
perihelie <archibaldmk4@gmail.com>
2025-10-08 13:28:38 +0200

    localing but giving up

--- hosts/perikon/modules/locales.nix
+++ hosts/perikon/modules/locales.nix
@@ -1,14 +1,13 @@
 {
    i18n.defaultLocale = "en_US.UTF-8";
-   i18n.extraLocales = [
-      "fr_FR.UTF-8/UTF-8"
-      "ja_JP.UTF-8/UTF-8"
-   ];
+   i18n.extraLocales = [ "fr_FR.UTF-8/UTF-8" "ja_JP.UTF-8/UTF-8" ];
 
    i18n.extraLocaleSettings = {
-      LC_CTYPE = "en_US.UTF-8";
+      LANGUAGE = "en_US.UTF-8";
+      # LC_CTYPE = "en_US.UTF-8";
+      # LC_COLLATE = "en_US.UTF-8";
       LC_ADDRESS = "fr_FR.UTF-8";
-      LC_MESSAGES = "en_US.UTF-8";
+      # LC_MESSAGES = "en_US.UTF-8";
       LC_IDENTIFICATION = "fr_FR.UTF-8";
       LC_MEASUREMENT = "ja_JP.UTF-8";
       LC_MONETARY = "fr_FR.UTF-8";
@@ -17,6 +16,7 @@
       LC_PAPER = "fr_FR.UTF-8";
       LC_TELEPHONE = "fr_FR.UTF-8";
       LC_TIME = "ja_JP.UTF-8";
+      # LC_ALL = "en_US.UTF-8";
    };
 
    time.timeZone = "Europe/Paris";
