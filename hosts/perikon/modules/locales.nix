e4f6b7c11d5576fb39e3e9ad84fa9a173239aa38
perihelie <archibaldmk4@gmail.com>
2025-10-20 22:03:53 +0200

    corrected locales

--- hosts/perikon/modules/locales.nix
+++ hosts/perikon/modules/locales.nix
@@ -1,6 +1,10 @@
 {
    i18n.defaultLocale = "en_US.UTF-8";
-   i18n.extraLocales = [ "fr_FR.UTF-8/UTF-8" "fr_CA.UTF-8" "ja_JP.UTF-8/UTF-8" ];
+   i18n.extraLocales = [
+      "en_US.UTF-8/UTF-8"
+      "fr_FR.UTF-8/UTF-8"
+      "ja_JP.UTF-8/UTF-8"
+   ];
 
    i18n.extraLocaleSettings = {
       LANGUAGE = "en_US.UTF-8";
@@ -12,7 +16,7 @@
       LC_NUMERIC = "ja_JP.UTF-8";
       LC_PAPER = "ja_JP.UTF-8";  
       LC_TELEPHONE = "fr_FR.UTF-8";
-      LC_TIME = "fr_CA.UTF-8";  
+      LC_TIME = "ja_JP.UTF-8";  
    };
 
    time.timeZone = "Europe/Paris";
