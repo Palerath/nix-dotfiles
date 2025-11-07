10af539f9dc9468b7e37789652f8f4d0719c5add
perihelie <archibaldmk4@gmail.com>
2025-11-07 17:18:11 +0100

    added vim config

--- common/vim.nix
+++ common/vim.nix
@@ -1,93 +1,49 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
-{
-    programs.vim = {
-        enable = true;
-
-        plugins = with pkgs.vimPlugins; [
-            tokyonight-nvim
-            fzf-vim
-            lightline-vim
-            vim-lsp
-        ];
-
-        extraConfig = ''
-      " Options
-      set number
-      set relativenumber
-
-      filetype plugin indent on
-      set expandtab
-      set shiftwidth=4
-      set softtabstop=4
-      set tabstop=4
-      set smartindent
-
-      set backspace=indent,eol,start
-
-      syntax on
-
-      " Keybinds
-      let mapleader = " "
-
-      nnoremap <leader>cd :Ex<CR>
-
-      " Colors
-      set termguicolors
-      let g:tokyonight_enable_italic = 1
-      colorscheme tokyonight
-
-      set laststatus=2
-      let g:lightline = {'colorscheme' : 'tokyonight'}
-
-      " FZF
-      nnoremap <leader>ff :Files<CR>
-      nnoremap <leader>fo :History<CR>
-      nnoremap <leader>fb :Buffers<CR>
-
-      nnoremap <leader>fg :Rg<Space>
-
-      " LSP Configuration
-      " Enable diagnostics highlighting
-      let lspOpts = #{autoHighlightDiags: v:true}
-      autocmd User LspSetup call LspOptionsSet(lspOpts)
-      let lspServers = [
-            \ #{
-            \   name: 'rust-analyzer',
-            \   filetype: ['rust'],
-            \   path: 'rust-analyzer',
-            \   args: []
-            \ }
-            \ ]
-
-      autocmd User LspSetup call LspAddServer(lspServers)
-
-      " LSP Key mappings
-      nnoremap gd :LspGotoDefinition<CR>
-      nnoremap gr :LspShowReferences<CR>
-      nnoremap K  :LspHover<CR>
-      nnoremap gl :LspDiag current<CR>
-      nnoremap <leader>nd :LspDiag next \| LspDiag current<CR>
-      nnoremap <leader>pd :LspDiag prev \| LspDiag current<CR>
-      inoremap <silent> <C-Space> <C-x><C-o>
-
-      " Set omnifunc for completion
-      autocmd FileType rust setlocal omnifunc=lsp#complete
-
-      " Custom diagnostic sign characters
-      autocmd User LspSetup call LspOptionsSet(#{
-          \   diagSignErrorText: '✘',
-          \   diagSignWarningText: '▲',
-          \   diagSignInfoText: '»',
-          \   diagSignHintText: '⚑',
-          \ })
-        '';
-    };
-
-    # Install additional tools needed for the setup
+let
+    vimConfigDir = ./.; # Points to the vim/ directory
+in
+    {
     environment.systemPackages = with pkgs; [
+        vim-full
         fzf
-        ripgrep  
+        ripgrep
         rust-analyzer
     ];
+
+    # Use environment.etc to link the config files from the Nix store
+    environment.etc = {
+        "xdg/vim/vimrc".source = "${vimConfigDir}/vimrc";
+        "xdg/vim/options.vim".source = "${vimConfigDir}/options.vim";
+        "xdg/vim/keybinds.vim".source = "${vimConfigDir}/keybinds.vim";
+        "xdg/vim/plugins.vim".source = "${vimConfigDir}/plugins.vim";
+        "xdg/vim/colors.vim".source = "${vimConfigDir}/colors.vim";
+        "xdg/vim/fzf.vim".source = "${vimConfigDir}/fzf.vim";
+        "xdg/vim/lsp.vim".source = "${vimConfigDir}/lsp.vim";
+    };
+
+    # Set vim as the default editor
+    environment.variables = {
+        EDITOR = "vim";
+        VISUAL = "vim";
+    };
+
+    # Activation script to symlink config to user home directories
+    system.userActivationScripts.vimConfig = ''
+    if [ ! -d "$HOME/.vim" ]; then
+      mkdir -p "$HOME/.vim"
+    fi
+
+    # Symlink all config files
+    ln -sf /etc/xdg/vim/vimrc "$HOME/.vimrc"
+    ln -sf /etc/xdg/vim/options.vim "$HOME/.vim/options.vim"
+    ln -sf /etc/xdg/vim/keybinds.vim "$HOME/.vim/keybinds.vim"
+    ln -sf /etc/xdg/vim/plugins.vim "$HOME/.vim/plugins.vim"
+    ln -sf /etc/xdg/vim/colors.vim "$HOME/.vim/colors.vim"
+    ln -sf /etc/xdg/vim/fzf.vim "$HOME/.vim/fzf.vim"
+    ln -sf /etc/xdg/vim/lsp.vim "$HOME/.vim/lsp.vim"
+
+    # Update vimrc to point to the correct locations
+    mkdir -p "$HOME/.vim/plugged"
+    '';
 }
---
+++ common/vim/colors.vim
@@ -0,0 +1,6 @@
+set termguicolors
+let g:tokyonight_enable_italic = 1
+colorscheme tokyonight
+
+set laststatus=2
+let g:lightline = {'colorscheme' : 'tokyonight'}
