66cdae7fe07bc4a126ef724670020d2e41d3dbf1
Sullamite <estellekoagne@gmail.com>
2025-12-15 15:45:42 +0100

    addded allRefs=true;

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
     inputs = {
         self.submodules = true;
-        self.allRefs = true;
+        allRefs = true;
         nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
         nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
