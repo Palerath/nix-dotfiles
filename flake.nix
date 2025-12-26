be9b22ebd1b4568b35928bb39cd4d82a1b835c8b
perihelie <archibaldmk4@gmail.com>
2025-12-26 14:44:44 +0100

    update: 2025-12-26T14:44:41+01:00

--- flake.nix
+++ flake.nix
@@ -1,131 +1,132 @@
 {
-    description = "Multi-host NixOS configuration";
+  description = "Multi-host NixOS configuration";
 
-    inputs = {
-        self.submodules = true;
+  inputs = {
+    self.submodules = true;
 
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-        nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-25.11";
 
-        home-manager = {
-            url = "github:nix-community/home-manager";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        home-manager-stable = {
-            url = "github:nix-community/home-manager/release-25.11";
-            inputs.nixpkgs.follows = "nixpkgs-stable";
-        };
-
-        flake-parts.url = "github:hercules-ci/flake-parts";
-        determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
-
-        nvf = {
-            url = "github:notashelf/nvf";
-            inputs.nixpkgs.follows = "nixpkgs";
-        };
-
-        zen-browser = {
-            url = "github:0xc000022070/zen-browser-flake";
-            inputs.nixpkgs.follows = "nixpkgs";
-            inputs.home-manager.follows = "home-manager";
-        };
-
-        hyprland.url = "github:hyprwm/Hyprland";
-
-        hyprland-plugins = {
-            url = "github:hyprwm/hyprland-plugins";
-            inputs.hyprland.follows = "hyprland";
-        };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    outputs =
-        inputs@{
-        self,
-        flake-parts,
-        nixpkgs,
-        nixpkgs-stable,
-        home-manager,
-        home-manager-stable,
-        determinate,
-        ...
-        }:
-        flake-parts.lib.mkFlake { inherit inputs; } {
-            systems = [ "x86_64-linux" ];
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
 
-            flake = {
-                # User configurations
-                userConfigs = {
-                    perihelie = import (self + /users/perihelie/home.nix);
-                    estelle = import (self + /users/estelle/home.nix);
-                };
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
 
-                # Helper function to create host configurations
-                lib.mkHost =
-                    {
-                    hostName,
-                    system ? "x86_64-linux",
-                    useStable ? false,
-                    }:
-                    let
-                        # Choose nixpkgs and home-manager based on stability preference
-                        pkgsInput = if useStable then nixpkgs-stable else nixpkgs;
-                        hmInput = if useStable then home-manager-stable else home-manager;
-                    in
-                        pkgsInput.lib.nixosSystem {
-                            inherit system;
-                            specialArgs = {
-                                inherit inputs;
-                                inherit hostName;
-                                userConfigs = self.userConfigs;
-                            };
-                            modules = [
-                                hmInput.nixosModules.home-manager
-                                {
-                                    _module.args = { inherit inputs; };
-                                    home-manager.extraSpecialArgs = { inherit inputs hostName; };
-                                }
-                                (self + /hosts/${hostName}/hardware-configuration.nix)
-                                (self + /hosts/${hostName}/configuration.nix)
-                                # Optionally add common modules
-                                self.nixosModules.common
-                                determinate.nixosModules.default
-                            ];
-                        };
+    nvf = {
+      url = "github:notashelf/nvf";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
 
-                # Define each host using the helper
-                nixosConfigurations = {
-                    perikon = self.lib.mkHost { hostName = "perikon"; };
-                    latitude = self.lib.mkHost { hostName = "latitude"; };
-                    linouce = self.lib.mkHost {
-                        hostName = "linouce";
-                        useStable = true;
-                    };
-                };
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
 
-                # Common modules
-                nixosModules = {
-                    common = import (self + /common);
-                };
-            };
+    hyprland.url = "github:hyprwm/Hyprland";
 
-            perSystem =
-                { pkgs, ... }:
-                {
-                    devShells = {
-                        default = pkgs.mkShell {
-                            buildInputs = with pkgs; [
-                                git
-                                nh
-                                nixos-rebuild
-                            ];
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+  };
 
-                            shellHook = ''
-                echo "NixOS development environment loaded"
-                echo "Tools: nh, nixos-rebuild, git"
-                            '';
-                        };
-                    };
-                };
+  outputs = inputs @ {
+    self,
+    flake-parts,
+    nixpkgs,
+    nixpkgs-stable,
+    home-manager,
+    home-manager-stable,
+    determinate,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+
+      flake = {
+        # User configurations
+        userConfigs = {
+          perihelie = import (self + /users/perihelie/home.nix);
+          estelle = import (self + /users/estelle/home.nix);
         };
+
+        # Helper function to create host configurations
+        lib.mkHost = {
+          hostName,
+          system ? "x86_64-linux",
+          useStable ? false,
+        }: let
+          # Choose nixpkgs and home-manager based on stability preference
+          pkgsInput =
+            if useStable
+            then nixpkgs-stable
+            else nixpkgs;
+          hmInput =
+            if useStable
+            then home-manager-stable
+            else home-manager;
+        in
+          pkgsInput.lib.nixosSystem {
+            inherit system;
+            specialArgs = {
+              inherit inputs;
+              inherit hostName;
+              userConfigs = self.userConfigs;
+            };
+            modules = [
+              hmInput.nixosModules.home-manager
+              {
+                _module.args = {inherit inputs;};
+                home-manager.extraSpecialArgs = {inherit inputs hostName;};
+              }
+              (self + /hosts/${hostName}/hardware-configuration.nix)
+              (self + /hosts/${hostName}/configuration.nix)
+              # Optionally add common modules
+              self.nixosModules.common
+              determinate.nixosModules.default
+            ];
+          };
+
+        # Define each host using the helper
+        nixosConfigurations = {
+          perikon = self.lib.mkHost {hostName = "perikon";};
+          latitude = self.lib.mkHost {hostName = "latitude";};
+          linouce = self.lib.mkHost {
+            hostName = "linouce";
+            useStable = true;
+          };
+        };
+
+        # Common modules
+        nixosModules = {
+          common = import (self + /common);
+        };
+      };
+
+      perSystem = {pkgs, ...}: {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              git
+              nh
+              nixos-rebuild
+            ];
+
+            shellHook = ''
+              echo "NixOS development environment loaded"
+              echo "Tools: nh, nixos-rebuild, git"
+            '';
+          };
+        };
+      };
+    };
 }
