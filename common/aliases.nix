b365105b47113a3dce8493ced34a7373ae37f23d
perihelie <archibaldmk4@gmail.com>
2026-01-07 13:28:32 +0100

    edited commands

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,34 +1,38 @@
-{ hostName, username }:
 {
-    kumit = "bash /home/${username}/dotfiles/scripts/kumit.sh";
-    rebuild-nix = "sudo nixos-rebuild switch --flake 'path:/home/${username}/dotfiles#${hostName}'";
-    rebuild = "nh os switch /home/${username}/dotfiles -H ${hostName}";
+  hostName,
+  username,
+}: let
+  dotifilesPath = "/home/${username}/dotfiles";
+in {
+  kumit = "bash ${dotifilesPath}/scripts/kumit.sh";
+  rebuild-nix = "sudo nixos-rebuild switch --flake 'path:'${dotifilesPath}#${hostName}'";
+  rebuild = "nh os switch ${dotifilesPath} -H ${hostName}";
 
-    update-flakes = "nix flake update && kumit 'update flakes.lock'";
-    maj = "z dotfiles && git pull && git submodule update --remote && update-flakes && rebuild";
+  update-flakes = "nix flake update && kumit 'update flakes.lock'";
+  maj = "cd ${dotifilesPath} && git pull && git submodule update --remote && update-flakes && rebuild";
 
-    # Git aliases
-    g = "git";
-    ga = "git add .";
-    gc = "git commit -am";
-    gco = "git checkout";
-    gd = "git diff";
-    gl = "git log";
-    gp = "git push";
-    gpl = "git pull";
-    gs = "git status";
-    gb = "git branch";
+  # Git aliases
+  g = "git";
+  ga = "git add .";
+  gc = "git commit -am";
+  gco = "git checkout";
+  gd = "git diff";
+  gl = "git log";
+  gp = "git push";
+  gpl = "git pull";
+  gs = "git status";
+  gb = "git branch";
 
-    # Common system aliases
-    ls = "eza";
-    ll = "eza -la";
-    la = "eza -la";
-    l = "eza -l";
-    ".." = "z ..";
-    "..." = "z ../..";
+  # Common system aliases
+  ls = "eza";
+  ll = "eza -la";
+  la = "eza -la";
+  l = "eza -l";
+  ".." = "z ..";
+  "..." = "z ../..";
 
-    core-ls = "ls";
+  core-ls = "ls";
 
-    # Zoxide aliases
-    zi = "zi";
+  # Zoxide aliases
+  zi = "zi";
 }
