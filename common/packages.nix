a96d251653e4168eb6e909732e32d437766a1a1c
perihelie <archibaldmk4@gmail.com>
2026-01-19 22:27:40 +0100

    beeg reorganization

--- common/packages.nix
+++ common/packages.nix
@@ -1,23 +1,22 @@
-{pkgs, ...}:
-{
-    environment.systemPackages = with pkgs; [ 
-        wl-clipboard
-        uutils-coreutils-noprefix
-        ffmpeg-full
-        tree
-        wget
-        ripgrep
-        fd
-        bat
-        eza
-        zoxide
-        fzf
-        file
-        unzip
-        p7zip
-        toybox
-        net-tools
-      ];
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    uutils-coreutils-noprefix
+    ffmpeg-full
+    tree
+    wget
+    ripgrep
+    fd
+    bat
+    eza
+    zoxide
+    fzf
+    file
+    unzip
+    p7zip
+    toybox
+    net-tools
+  ];
 
-      programs.nh.enable = true;
+  programs.nh.enable = true;
 }
