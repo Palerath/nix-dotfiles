9b457e04d4479b774545bccc4a487ff16818fa25
perihelie <archibaldmk4@gmail.com>
2026-02-24 17:41:42 +0100

    removed hy3

--- flake.nix
+++ flake.nix
@@ -40,13 +40,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hy3 = {
-      url = "github:outfoxxed/hy3?ref=hl{version}"; # where {version} is the hyprland release version
-      # or "github:outfoxxed/hy3" to follow the development branch.
-      # (you may encounter issues if you dont do the same for hyprland)
-      inputs.hyprland.follows = "hyprland";
-    };
-
     #  aagl = {
     #    url = "github:ezKEa/aagl-gtk-on-nix";
     #    inputs.nixpkgs.follows = "nixpkgs";
