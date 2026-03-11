ee6c6779250b648e8b45d1f792c6a7dc81f6f043
Perihelie <archibaldmk4@gmail.com>
2026-03-11 21:16:27 +0100

    aliases

--- common/aliases.nix
+++ common/aliases.nix
@@ -1,44 +1,52 @@
 {
   hostName,
   username,
+  isDarwin ? false,
 }: let
-  # Define dotfiles path based on hostname
   dotfilesPath =
     if hostName == "periserver"
     then "/opt/dotfiles"
     else "/home/${username}/dotfiles";
+
+  rebuildCmd =
+    if isDarwin
+    then "darwin-rebuild switch --flake 'path:${dotfilesPath}#${hostName}'"
+    else "nh os switch ${dotfilesPath} -H ${hostName}";
+
+  rebuildNixCmd =
+    if isDarwin
+    then "sudo darwin-rebuild switch --flake 'path:${dotfilesPath}#${hostName}'"
+    else "sudo nixos-rebuild switch --flake 'path:${dotfilesPath}#${hostName}'";
 in {
-  kumit = "bash ${dotfilesPath}/scripts/kumit.sh";
-  rebuild-nix = "sudo nixos-rebuild switch --flake 'path:${dotfilesPath}#${hostName}'";
-  rebuild = "nh os switch ${dotfilesPath} -H ${hostName}";
+  shellAliases = {
+    kumit = "bash ${dotfilesPath}/scripts/kumit.sh";
+    rebuild-nix = rebuildNixCmd;
+    rebuild = rebuildCmd;
 
-  update-flakes = "cd ${dotfilesPath} && nix flake update && kumit 'update flakes.lock'";
-  maj = "cd ${dotfilesPath} && git pull && git submodule update --remote && update-flakes && rebuild";
+    update-flakes = "cd ${dotfilesPath} && nix flake update && kumit 'update flakes.lock'";
+    maj = "cd ${dotfilesPath} && git pull && git submodule update --remote && update-flakes && rebuild";
 
-  dot = "z dot && tmux";
+    dot = "z dot && tmux";
 
-  # Git aliases
-  g = "git";
-  ga = "git add .";
-  gc = "git commit -am";
-  gco = "git checkout";
-  gd = "git diff";
-  gl = "git log";
-  gp = "git push";
-  gpl = "git pull";
-  gs = "git status";
-  gb = "git branch";
+    g   = "git";
+    ga  = "git add .";
+    gc  = "git commit -am";
+    gco = "git checkout";
+    gd  = "git diff";
+    gl  = "git log";
+    gp  = "git push";
+    gpl = "git pull";
+    gs  = "git status";
+    gb  = "git branch";
 
-  # Common system aliases
-  ls = "eza";
-  ll = "eza -la";
-  la = "eza -la";
-  l = "eza -l";
-  ".." = "z ..";
-  "..." = "z ../..";
+    ls  = "eza";
+    ll  = "eza -la";
+    la  = "eza -la";
+    l   = "eza -l";
+    ".."  = "z ..";
+    "..." = "z ../..";
 
-  core-ls = "ls";
-
-  # Zoxide aliases
-  zi = "zi";
+    core-ls = "ls";
+    zi = "zi";
+  };
 }
