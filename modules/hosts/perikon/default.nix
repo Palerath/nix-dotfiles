f103069d470e2c530a2e2b8e56e0fcc756a2b71c
perihelie <archibaldmk4@gmail.com>
2026-03-25 17:28:10 +0100

    proper inputs

--- modules/hosts/perikon/default.nix
+++ modules/hosts/perikon/default.nix
@@ -4,6 +4,10 @@
   ...
 }: {
   flake.nixosConfigurations.perikon = inputs.nixpkgs.lib.nixosSystem {
+    specialArgs = {
+      inherit (inputs) hyprland aagl zen-browser;
+      inherit self;
+    };
     modules = [
       self.nixosModules.perikonConfiguration
       self.nixosModules.perihelieConfiguration
