3946c1ce291061587f99b3cdb48034be1f9d28ab
perihelie <archibaldmk4@gmail.com>
2026-03-25 14:50:02 +0100

    hypring

--- flake.nix
+++ flake.nix
@@ -6,6 +6,45 @@
     import-tree.url = "github:vic/import-tree";
 
     wrapper-modules.url = "github:BirdeeHub/nix-wrapper-modules";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+
+    nix-darwin = {
+      url = "github:nix-darwin/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager-stable = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs-stable";
+    };
+
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    zen-browser = {
+      url = "github:0xc000022070/zen-browser-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
+
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      inputs.hyprland.follows = "hyprland";
+    };
+
+    aagl = {
+      url = "github:ezKEa/aagl-gtk-on-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs: inputs.flake-parts.lib.mkFlake {inherit inputs;} (inputs.import-tree ./modules);
--- modules/programs/hypr.nix
+++ modules/programs/hypr.nix
@@ -17,7 +17,7 @@
       waylandCompositors = {
         hyprland = {
           prettyName = "Hyprland";
-          binPath = "${pkgs.hyprland}";
+          binPath = "/run/current-system/sw/bin/Hyprland";
         };
       };
     };
@@ -27,77 +27,14 @@
     pkgs,
     lib,
     self',
+    inputs',
     ...
   }: {
-    packages.myHyprland = inputs.hyprland.lib.wrap {
+    packages.myHyprland = inputs.wrapper-modules.wrappers.hyprland.wrap {
       inherit pkgs;
       settings = {
-        "$mod" = "SUPER";
-        "$terminal" = "kitty";
-        "$menu" = "wofi --show drun";
-
-        monitor = [
-          "DP-1,2560x1440@75,0x0,1"
-          "HDMI-A-2,1920x1080@60,320x1440,1"
-        ];
-
-        exec-once = [
-          "uwsm app -- fcitx5"
-          "uwsm app -- mako"
-          "uwsm app -- waybar"
-          "hyprctl setcursor Adwaita 24"
-        ];
-
-        input = {
-          kb_layout = "qwerty-fr";
-          follow_mouse = 1;
-        };
-
-        env = [
-          "LIBVA_DRIVER_NAME,nvidia"
-          "GBM_BACKEND,nvidia-drm"
-          "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-          "NVD_BACKEND,direct"
-        ];
-
-        general = {
-          gaps_in = 5;
-          gaps_out = 5;
-          border_size = 2;
-          "col.active_border" = "rgba(4c7899ff)";
-          layout = "dwindle";
-        };
-
-        # Bindings
-        bind =
-          [
-            "$mod, Return, exec, uwsm app -- $terminal"
-            "$mod, D, exec, uwsm app -- $menu"
-            "$mod SHIFT, Q, killactive"
-            "$mod, F, fullscreen"
-            # Focus
-            "$mod, H, movefocus, l"
-            "$mod, L, movefocus, r"
-            "$mod, K, movefocus, u"
-            "$mod, J, movefocus, d"
-          ]
-          ++ (
-            # Workspace binds
-            builtins.concatLists (builtins.genList (
-                i: let
-                  ws = i + 1;
-                in [
-                  "$mod, code:1${toString i}, workspace, ${ws}"
-                  "$mod SHIFT, code:1${toString i}, movetoworkspace, ${ws}"
-                ]
-              )
-              9)
-          );
-
-        bindm = [
-          "$mod, mouse:272, movewindow"
-          "$mod, mouse:273, resizewindow"
-        ];
+        package = inputs'.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+        portalPackage = inputs'.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
       };
     };
   };
