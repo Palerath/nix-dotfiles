0a22e626386cc93726b6a4da6073f80012d5554f
perihelie <archibaldmk4@gmail.com>
2026-03-28 18:01:46 +0100

    update: 2026-03-28T18:01:40+01:00

--- modules/features/sops.nix
+++ modules/features/sops.nix
@@ -9,7 +9,7 @@
     ...
   }: {
     imports = [inputs.sops-nix.nixosModules.sops];
-    environment.systemPackages = with pkgs; [ssh-to-age];
+    environment.systemPackages = with pkgs; [sops age ssh-to-age];
 
     sops = {
       age.sshKeyPaths = ["/etc/ssh/ssh_host_ed25519_key"];
