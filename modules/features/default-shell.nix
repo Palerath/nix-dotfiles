30e69e7d69e410f4fd68c084323f6a536886fa51
perihelie <archibaldmk4@gmail.com>
2026-03-28 12:02:47 +0100

    nushell

--- modules/features/default-shell.nix
+++ modules/features/default-shell.nix
@@ -88,6 +88,56 @@
     };
   };
 
+  flake.homeModules.nushell = {
+    hostName,
+    config,
+    pkgs,
+    ...
+  }: let
+    username = config.home.username;
+    sharedAliases = import self.nixosModules.commonAliases {inherit hostName username;};
+    # Function to generate Nushell alias lines
+    nushellAliases = builtins.concatStringsSep "\n" (
+      builtins.attrValues (
+        builtins.mapAttrs (name: value: "alias ${name} = ${value}") sharedAliases.shellAliases
+      )
+    );
+  in {
+    home.packages = with pkgs; [
+      carapace
+    ];
+
+    programs.nushell = {
+      enable = true;
+
+      # Shell aliases
+      shellAliases = sharedAliases.shellAliases;
+
+      extraConfig = ''
+        # Direct alias integration
+           ${nushellAliases}
+
+        # fastfetch
+        fastfetch
+
+        # Core environment settings
+        $env.config = {
+          show_banner: false
+          completions: {
+            algorithm: "fuzzy"
+            case_sensitive: false
+            quick: true
+            partial: true
+          }
+        edit_mode: "vi"
+        }
+      '';
+    };
+
+    programs.eza.enableNushellIntegration = true;
+    programs.carapace.enableNushellIntegration = true;
+  };
+
   flake.homeModules.fastfetch = {config, ...}: {
     programs.fastfetch = {
       enable = true;
